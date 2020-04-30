2b22216be758763915c4a6c3f112889ab4957e90
Datafluisteraar <steven.gort@ictu.nl>
2020-04-30 15:33:32 +0200

    configured sample bvv rules

--- src/bvv.js
+++ src/bvv.js
@@ -1,39 +1,79 @@
-const fastify = require("fastify");
-const app = fastify();
+import React, { Component } from "react";
+import TextFileReader from './components/text-file-reader';
+import { Engine } from 'json-rules-engine';
+const BvvRules = require('./bvv-rules.txt');
 
-const { Engine } = require("json-rules-engine");
+/**
+ * Setup a new engine
+ */
+let engine = new Engine()
 
-let engine = new Engine();
-
+// define a rule for detecting the player has exceeded foul limits.  Foul out any player who:
+// (has committed 5 fouls AND game is 40 minutes) OR (has committed 6 fouls AND game is 48 minutes)
 engine.addRule({
-  conditions: {
-    all: [
-      {
-        fact: "temperature",
-        operator: "equal",
-        value: 100
-      }
-    ]
-  },
-  onSuccess(){
-    console.log('on success called')
-  },
-  onFailure(){
-    console.log('on failure called')
-  },
-  event: {
-    type: "message",
-    params: {
-      data: "hello-world!"
+    conditions: {
+        any: [{
+            all: [{
+                fact: 'gameDuration',
+                operator: 'equal',
+                value: 40
+            }, {
+                fact: 'personalFoulCount',
+                operator: 'greaterThanInclusive',
+                value: 5
+            }]
+        }, {
+            all: [{
+                fact: 'gameDuration',
+                operator: 'equal',
+                value: 48
+            }, {
+                fact: 'personalFoulCount',
+                operator: 'greaterThanInclusive',
+                value: 6
+            }]
+        }]
+    },
+    event: {  // define the event to fire when the conditions evaluate truthy
+        type: 'fouledOut',
+        params: {
+            message: 'Player has fouled out!'
+        }
     }
-  }
-});
+})
 
-const facts = { temperature: 100 };
+/**
+ * Define facts the engine will use to evaluate the conditions above.
+ * Facts may also be loaded asynchronously at runtime; see the advanced example below
+ */
+let facts = {
+    personalFoulCount: 6,
+    gameDuration: 40
+}
 
-engine
-  .run(facts)
-  .then(results => {
-    results.events.map(event => console.log('value', event.params.data));
-  })
-  .catch((error)=> console.log('err is', error));
+function doEngineSession() {
+    // Run the engine to evaluate
+    engine
+        .run(facts)
+        .then(results => {
+            // 'results' is an object containing successful events, and an Almanac instance containing facts
+            results.events.map(event => alert(event.params.message))
+        })
+}
+
+class Rules extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Rules Engine config</h1>
+                <TextFileReader
+                    txt={BvvRules}
+                />
+                <p></p>
+                <p className="Check Rules"><button onClick={doEngineSession}>Check rules!</button></p>
+            </div>
+        )
+    }
+}
+
+export default Rules;
